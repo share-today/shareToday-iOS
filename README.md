b6c69da32a5599f1c48a604fb322b1daa988d31e
hyeri00 <nexusring@gmail.com>
2023-12-21 16:38:58 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# shareToday-iOS
---
+++ shareToday-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shareToday-iOS
+//
+//  Created by 혜리 on 12/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
