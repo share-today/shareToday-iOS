359840013710828489fef535fb25cf771b613d86
hyeri00 <nexusring@gmail.com>
2023-12-21 21:18:50 +0900

    title, login text 추가

--- shareToday-iOS/DesignSystem/Values/TextManager.swift
+++ shareToday-iOS/DesignSystem/Values/TextManager.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct TextManager {
+    
+    // MARK: - Main
+    
+    static let title = "하루 공유"
+    
+    // MARK: - Login
+    
+    static let kakao = "카카오톡으로 로그인"
+    static let apple = "Apple로 로그인"
+    static let google = "Google로 로그인"
+}
