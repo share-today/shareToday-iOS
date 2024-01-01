13e179257d240ab898531e764bacbba529dbc075
hyeri00 <nexusring@gmail.com>
2024-01-01 23:47:44 +0900

    AuthRepositoryImpl 생성

--- shareToday-iOS/Repository/Auth/AuthRepository.swift
+++ shareToday-iOS/Repository/Auth/AuthRepository.swift
@@ -10,3 +10,45 @@ import Foundation
 protocol AuthRepository {
     func signIn(req: AuthEntity.Request) async throws -> AuthEntity.Response
 }
+
+final class AuthRepositoryImpl: AuthRepository {
+
+    func signIn(req: AuthEntity.Request) async throws -> AuthEntity.Response {
+
+        do {
+            let result = try await OBNetworkManager
+                .shared
+                .asyncRequest(
+                    object: AuthDTO.self,
+                    router: APIRouter.auth(
+                        body: AuthRequest.Body(
+                            sns: req.sns,
+                            snsId: req.snsId,
+                            email: req.email,
+                            accessToken: req.accessToken
+                        ).encode()
+                    )
+                )
+
+            guard let data = result.value else {
+                throw NetworkError.noExist
+            }
+
+            return data.toDomain
+
+        } catch {
+            print(error.localizedDescription)
+
+            throw error
+        }
+    }
+}
+
+extension AuthDTO {
+    var toDomain: AuthEntity.Response {
+        return AuthEntity.Response(
+            result: self.result,
+            data: self.data
+        )
+    }
+}
