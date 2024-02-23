6a5c89a7a3e171a6aee9cc0a4417f65037f8b48d
hyeri00 <nexusring@gmail.com>
2024-02-24 06:09:38 +0900

    [SHA-9] 홈 화면 내비게이션바 생성

--- shareToday-iOS/Presentation/Main/MainViewController.swift
+++ shareToday-iOS/Presentation/Main/MainViewController.swift
@@ -20,4 +20,37 @@ final class MainViewController: UIViewController {
         
         self.view = mainView
     }
+    
+    // MARK: - Initialize
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        let image = Icon.arrow_left.image?.withTintColor(Colors.black, renderingMode: .alwaysOriginal)
+        
+        navigationController?.navigationBar.barTintColor = Colors.white
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image, style: .done,
+            target: self, action: #selector(showPrevious))
+    }
+    
+    @objc
+    private func showPrevious() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
