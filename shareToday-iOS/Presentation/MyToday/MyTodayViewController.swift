c8b4a6c35967d41ef34e179643535161f3cd67b8
hyeri00 <nexusring@gmail.com>
2024-02-24 07:57:19 +0900

    [SHA-10] 키보드 내림 제스처 추가

--- shareToday-iOS/Presentation/MyToday/MyTodayViewController.swift
+++ shareToday-iOS/Presentation/MyToday/MyTodayViewController.swift
@@ -20,4 +20,8 @@ final class MyTodayViewController: UIViewController {
         
         self.view = myTodayView
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
