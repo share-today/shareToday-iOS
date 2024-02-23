d4b05b1dffc5fb775902960f7f096aad3b31715e
hyeri00 <nexusring@gmail.com>
2024-02-24 07:32:28 +0900

    [SHA-10] MyTodayView load

---
+++ shareToday-iOS/Presentation/MyToday/MyTodayViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyTodayViewController.swift
+//  shareToday-iOS
+//
+//  Created by 혜리 on 2/24/24.
+//
+
+import UIKit
+
+final class MyTodayViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let myTodayView = MyTodayView()
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.view = myTodayView
+    }
+}
